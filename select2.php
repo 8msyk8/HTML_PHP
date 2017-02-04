9a0069b0b624b914dbadbb7d7cbd923068ae6a34
masayuki <test>
2017-02-04 12:17:53 +0900

    added insert data

--- select2.php
+++ select2.php
@@ -3,26 +3,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-<title>MariaDBへの接続テスト</title>
+<title>入水量管理結果</title>
 </head>
 <body>
-
-<SCRIPT TYPE="text/javascript">
-<!--
-//――――――――――――――――――――――――――――――――――――――
-// 作成者 るび～/ACCESS R http://www5e.biglobe.ne.jp/~access_r/
-//――――――――――――――――――――――――――――――――――――――
-function DataReceive(){
-		//?以降の文字を取得する
-	var data = location.search.substring(1, location.search.length);
-		//エスケープされた文字をアンエスケープする
-	data = unescape(data);
-		//アラートで?以降の文字を表示する
-	alert(data);
-}
-window.onload = DataReceive;
-//-->
-</SCRIPT>
 <?php
 $date=$_GET['date'];
 $name=$_GET['name'];
@@ -37,15 +20,20 @@ print $name;
 print $place;
 print $amount;
 //print $dateFormat1;
-print $dateFormat2;
+//print $dateFormat2;
+//'".$_POST['_key1']."','"$_POST['_key2']."'
+//data input
+$db=mysql_select_db('wateramount', $con);
+//$query = "INSERT INTO inputdata(date,name)VALUES('$dateFormat2','')";
+$query = "INSERT INTO inputdata(date,name,place,amount)VALUES('$dateFormat2','$name','$place','$amount')";
+$result = mysql_query($query); 
+?>
 
-
+<?php
 mysql_select_db('wateramount', $con);
 $quryset = mysql_query('SET NAMES utf8', $con);
 $quryset = mysql_query('SELECT * FROM table1');
- 
 //http://mikako.pupu.jp/?p=1142
-
 print "<table border='1'>";
 print "<tr>";
 print "<th>名前</th>";
@@ -59,7 +47,7 @@ print "<th>場所/日付</th>";
   }
   print "</tr>";
   print "</table>";
-?>
+  ?>
 
 
 
